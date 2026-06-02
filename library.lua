664020a90914141e9835ba15a66b9856ac73c7f7
dopamine-wtf <bentonfent69@outlook.com>
2026-06-02 10:47:58 -0500

    Update library.lua

--- library.lua
+++ library.lua
@@ -234,7 +234,7 @@ local Library do
             ["Text"] = FromRGB(235, 235, 235),
             ["Text Stroke"] = FromRGB(0, 0, 0),
             ["Placeholder Text"] = FromRGB(185, 185, 185),
-            ["Accent"] = FromRGB(255, 80, 80)
+            ["Accent"] = FromRGB(0, 191, 255)
         }
     }
 
